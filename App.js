6763eddc93fb61aef13a54487bc84041e961f0be
eric97li <eric97li@gmail.com>
2021-02-20 12:49:08 -0600

    sign up and login and screens of general layout for app

--- App.js
+++ App.js
@@ -1,14 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { NativeAppEventEmitter, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Profile from "./Profile";
+import Home from "./Home";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      username: "",
+      password: ""
+    }
+
+    this.setUsername = this.setUsername.bind(this);
+    this.setPassword = this.setPassword.bind(this);
+  }
+
+  setUsername(y) {
+    this.setState({username: y})
+  }
+
+  setPassword(x) {
+    this.setState({password: x})
+  }
+
+
+  createTabNavigator(props) {
+    const Tab = createBottomTabNavigator();
+    return (
+      <Tab.Navigator>
+      <Tab.Screen name="Profile" options={{
+        tabBarIcon: () => {
+          let iconName = `md-person`;
+          return <Ionicons name={iconName} size={25}/>;
+        }
+      }}>
+        {props => <Profile {...props} username={this.state.username} password={this.state.password}/>}</Tab.Screen>
+    <Tab.Screen name="Home" options={{
+        tabBarIcon: () => {
+          let iconName = `md-home`;
+          return <Ionicons name={iconName} size={25}/>;
+        }
+      }}>{props => <Home {...props} username={this.state.username} />}</Tab.Screen>
+    </Tab.Navigator>
+    );
+  }
+
+  render() { 
+
+    return(
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name = "Login"
+          options={{title: 'Badger Bytes'}}
+        >
+          {props => <Login {...props} setUsernameCallBack={this.setUsername} setPasswordCallBack={this.setPassword}/>}
+          </Stack.Screen>
+        <Stack.Screen 
+          name="Sign Up"
+        >
+          {props => <SignUp {...props} setUsernameCallBack={this.setUsername} setPasswordCallBack={this.setPassword}/>} 
+        </Stack.Screen>
+        <Stack.Screen name="Badger Bytes"> 
+          {(props)=>this.createTabNavigator(props)}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+    
+   
   );
+
+  }
 }
 
 const styles = StyleSheet.create({
